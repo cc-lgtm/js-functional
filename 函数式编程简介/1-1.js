4b653817f51563e0e90f69c4fbb65cfd6f011b3a
litterbird <litterbird@ubuntu20.1>
2021-02-04 22:31:18 +0800

    js函数式编程学习记录

---
+++ 函数式编程简介/1-1.js
@@ -0,0 +1,30 @@
+let _ = require('underscore')
+
+_.each([1, 2, 3], print)
+function print(value) {
+    console.log(value)  //1  2  3
+}
+
+
+function splat(fn) {
+    return function(array) {
+        return fn.apply(null, array)
+    }
+}
+let addArrayElements = splat(function(x, y) { return x + y })
+let result = addArrayElements([1, 2])
+console.log(result)   //3
+
+
+function unsplat(fn) {
+    return function() {
+        return fn.call(null, _.toArray(arguments))
+    }
+}
+let joinElements = unsplat(function(array) { return array.join(' ') })
+let result1 = joinElements(1, 2)
+console.log(result1)  //1  2
+let result2 = joinElements('@', '#', '$', '%')
+console.log(result2)  //@  #  $  %
+
+
